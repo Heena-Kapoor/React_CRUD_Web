728c625dd1f0a1f077866526c957c7d8d759fe47
vaishnavi-kapoor <vaishnavikapoor561@gmail.com>
2026-02-16 14:43:01 +0530

    Creation of env file

--- react-crud/src/services/api.js
+++ react-crud/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3001/users'; // Mock API
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 export const fetchUsers = () => axios.get(API_BASE_URL);
 export const createUser = (user) => axios.post(API_BASE_URL, user);
